8fb668327258021919a7eda2e8df5b1458868e03
asdfgdghfjj <katakiyaharshl001@gmail.com>
2023-07-09 18:37:52 +0530

    Code Updated

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ app.use(express.json());
 app.use(require('./routerauth'));
 const PORT = process.env.PORT;
 
-app.get('/login', cors({ origin: 'https://graphical-password-system.onrender.com/' }), (req, res) => {
+app.get('/login', cors({ origin: 'https://graphical-password-system.vercel.app/' }), (req, res) => {
     res.send(`Login`);
 });
 
-app.get('/signup', cors({ origin: 'https://graphical-password-system.onrender.com/' }), (req, res) => {
+app.get('/signup', cors({ origin: 'https://graphical-password-system.vercel.app/' }), (req, res) => {
     res.send(`SignUp`);
 });
 
